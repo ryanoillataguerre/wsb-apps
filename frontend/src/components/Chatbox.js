a4811940b758c0c29f8ba234aa1a9a1e52ab969e
Derek Wu <d244wu@uwaterloo.ca>
2021-01-28 21:57:32 -0500

    Modularized the code a bit

--- frontend/src/components/Chatbox.js
+++ frontend/src/components/Chatbox.js
@@ -1,5 +1,39 @@
 import React from 'react';
 
+function ChatComment(props){
+  return(
+    <button className="chat-comment" onClick={function(){alert(props.user)}} >
+      {props.text}
+    </button>
+  );
+}
+
+class Chatbox extends React.Component{
+  
+  renderComment(){
+    return(
+      <ChatComment
+        user="Snoo"
+        text="Lorem ipsum dolor sit amet"
+      />
+    )
+  }
+
+  render(){
+    return (
+      <div className="chatbox-container">
+        {this.renderComment()}
+        <div className="chat-comment">Lorem ipsulor sit amet</div>
+        <div className="chat-comment">Lorem ipsm dolot amet</div>
+        <div className="chat-comment">Lorem ipsum dolor sit amet</div>
+        <div className="chat-comment">Lorem ipsum dolor siet</div>
+        <div className="chat-comment">Lorem ipsum dolor sit amet</div>
+        <div className="chat-comment">"Lorem ipsum dolor sit amet asdfasdaserasdf"</div>
+      </div>
+    )
+  }
+}
+/*
 const Chatbox = () => {
   return (
     <div className="chatbox-container">
@@ -13,5 +47,5 @@ const Chatbox = () => {
     </div>
   )
 }
-
+*/
 export default Chatbox;
